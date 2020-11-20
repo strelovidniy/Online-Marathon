a2f726de4d8e2e69295b9486b30745cb49855611
strelovidniy <dwordchanel@gmail.com>
2020-11-20 17:42:59 +0200

    Refactored.

--- Sprint_07/Task_02/MyProgram.cs
+++ Sprint_07/Task_02/MyProgram.cs
@@ -5,12 +5,10 @@ namespace Sprint_07.Task_02
 {
     internal class MyProgram
     {
-        public static double EvaluateAggregate
-        (
+        public static double EvaluateAggregate(
             double[] inputData, 
             Func<double, double, double> aggregate, 
-            Func<double, int, bool> predicate
-        )
+            Func<double, int, bool> predicate)
             => inputData.Where(predicate).Aggregate(aggregate);
     }
 }
