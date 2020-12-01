ef4aa5e502dae4e3105894181112141caaa59a6f
Roman Danylevych <roma.dan2001@gmail.com>
2020-12-01 22:44:49 +0200

    Refactored.

---
+++ Sprint_11/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sprint_11
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
